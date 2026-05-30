22dea88f130796e7aeca6d1124a980962a904109
Harshita <harshita93159@gmail.com>
2026-05-30 13:08:32 +0530

    Delete DAY_1 directory

--- DAY_1/yourname.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class yourname {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        System.out.println("HELLO MY NAME IS " + a);
-    }
-    
-}
